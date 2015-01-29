1c7a901ee1af2c8d4667bad59033dd0ece4b894b
Marissa Vega <marissavega90@gmail.com>
2015-01-29 15:52:15 -0600

    Created roll-dice with guesses and rolls displayed.

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,67 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return 'Hello Codeup';
+	// return View::make('hello');
 });
+
+Route::get('parks', function() {
+	return View::make('parks');
+});
+
+Route::post('parks.html', function() {
+	return 'Which will you see???????';
+});
+
+Route::get('say-hello/{name}', function($name) {
+		return View::make('sayhello');
+	
+	
+});
+
+Route::get('say-hello/{name}/{age}', function($name, $age) {
+	return "Hello $name! I hear you're $age years old.";
+});
+
+Route::get('resume', function() {
+	return "My resume";
+});
+
+Route::get('portfolio', function() {
+	return "My portfolio";
+});
+
+// NOTES:
+
+Route::get('say-hello/{urlName}', function($argName) {
+	return View::make('sayhello')->with('viewName', $argName)->with('age', 0);
+});
+
+
+Route::get('say-hello/{urlName}', function($argName) {
+	$data = array(
+		'viewName' => $argName,
+		'age'      => 0
+	);
+
+	// All three Views below are the same!
+
+	return View::make('sayhello', $data);
+	return View::make('sayhello')->with($data);
+	return View::make('sayhello')->with('viewName', $argName)->with('age', 0);
+});
+
+
+
+
+
+Route::get('/rolldice/{guess}', function($guess) {
+
+	$randomNumb = rand(1, 6);
+
+	return View::make('roll-dice')->with('randomNumb', $randomNumb)->with('guess', $guess);
+
+});
+
+
+
---
+++ app/views/roll-dice.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Roll Dice</title>
+</head>
+<body>
+	<h1>You rolled: <?= $randomNumb; ?></h1>
+	<h1>Your guess: <?= $guess; ?></h1>
+</body>
+</html>
